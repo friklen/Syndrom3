95b636a462e2d7099aae69f708916f1101e6f9d3
Elena Proskurina <friklen@gmail.com>
2021-12-16 23:50:57 +0400

    Created new test

--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -14,4 +14,13 @@ class StatisticServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldfindMaxSix() {
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 15, 8, 6, 11, 11, 12};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
 }
